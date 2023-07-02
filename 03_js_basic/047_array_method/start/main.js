3aacbe8bd8545a3f407ed33e2d2dc765e1568b2d
ky-ishida <k.ishida.official@gmail.com>
2023-07-02 18:29:03 +0900

    [feat]React.jsの使い方

--- 03_js_basic/047_array_method/start/main.js
+++ 03_js_basic/047_array_method/start/main.js
@@ -1,2 +1,18 @@
-const arry = [10, 20, 30, 40];
+const array = [10, 20, 30, 40];
 const newArry = [];
+
+for (let i = 0; i < array.length; i++) {
+  const val = array[i] * 2;
+  if(val>50){
+  newArry.push(array[i] * 2);
+}
+}
+
+console.log(newArry);
+
+const newArry2 = array.map((val) => {
+  return val * 2;
+});
+
+const newArry3 =newArry2.filter(val=> val>50)
+console.log(newArry2);
